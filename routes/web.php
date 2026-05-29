943ac2ec700ca3e853b92bab28733de876512f3c
alvanzip-hue <240514004@fellow.lpkia.ac.id>
2026-05-29 17:58:41 +0700

    Sesi 10

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\AnggotaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +17,7 @@ Route::get('/mahasiswa/edit/{id}', [MahasiswaController::class, 'edit']);
 Route::post('/mahasiswa/update/{id}', [MahasiswaController::class, 'update']);
 
 Route::get('/mahasiswa/delete/{id}', [MahasiswaController::class, 'destroy']);
+
+// Rute ini otomatis mencakup seluruh fungsi CRUD untuk Anggota.
+// Data Kartu Anggota ikut terkelola di dalam rute ini melalui controller terintegrasi.
+Route::resource('anggota', AnggotaController::class);
